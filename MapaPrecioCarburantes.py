c01d5189ce7d2a614474056e6a1d452e8765e5c1
jlluch <jlluch@ai2.upv.es>
2024-04-16 14:17:56 +0200

    Update MapaPrecioCarburantes.py

--- MapaPrecioCarburantes.py
+++ MapaPrecioCarburantes.py
@@ -153,7 +153,9 @@ provincia = display_prov_filter()
 
 df, prov_data, gdf, FAct = cargarFichero()
 
-dfprov = df[df.Provincia == provincia].reset_index()
+# dfprov = df[df.Provincia == provincia].reset_index()
+
+dfprov = df
 
 st.caption(APP_SUB_TITLE+'\n'+FAct)
 st.caption(APP_SUB_TITLE2)
@@ -194,7 +196,7 @@ if x>0:
 m = folium.Map(location=[latMap, lonMap], zoom_start=8,attr='LOL',max_bounds=True,min_zoom=5.5)
 
 for i in range(len(dfprov)):
-    folium.CircleMarker(location=[dfprov.Latitud.iat[i],dfprov.Longitud.iat[i],],popup=dfprov.data.iat[i],radius=10,color=dfprov.color.iat[i],fill=True, fill_opacity=0.7).add_to(m)
+    folium.Circle(location=[dfprov.Latitud.iat[i],dfprov.Longitud.iat[i],],popup=dfprov.data.iat[i],radius=10,color=dfprov.color.iat[i],fill=True, fill_opacity=0.7).add_to(m)
 
 # folium.Choropleth(geo_data=prov_geo,name="choropleth",data=prov_data,columns=["codigo", 'mean'],key_on="properties.codigo", fill_color="Greys",fill_opacity=0.4,line_opacity=1.0,legend_name="Precio medio: "+combustible).add_to(m)
 
